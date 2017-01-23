a99af92d41acd53fa2eb13d45f9de411712f1b63
Deepak Maurya <itsdpm@gmail.com>
2017-01-24 00:03:12 +0530

    Problem 0101, to be reconsidered later

--- Book1 : Up and Running/Practice Problem 0101.js
+++ Book1 : Up and Running/Practice Problem 0101.js
@@ -6,7 +6,22 @@ as long as your purchase amount is below your mental spending
 threshold.
 */
 
+var TAX_RATE = 0.15;
+
+function taxAmount(purchasePrice){
+    return purchasePrice*TAX_RATE;
+}
+
+function purchaseReciept(priceOfPhone, priceOfAccessory, purchasePrice, tax){
+    console.log("Phone : $", priceOfPhone.toPrecision(3));
+    console.log("Accessories : $", priceOfAccessory.toPrecision(3));
+    console.log("Total : $", purchasePrice.toPrecision(3));
+    console.log("Tax : $", tax.toPrecision(3));
+    console.log("Price after Taxes : $", (purchasePrice + tax).toPrecision(3));
+}
+
 var totalPrice = 0;
+var purchasePrice = 0;
 var priceOfPhone = 0;
 var priceOfAccessory = 0;
 var spendingThreshold = 0; // also the total expenditure on a purchase
@@ -14,19 +29,20 @@ var bankBalance = 0;
 
 bankBalance = prompt("What is your bank balance?");
 
-if(bankBalance!==0){
-    while(bankBalance===0){
+if(bankBalance!=0){
+    while(bankBalance!=0){
         
-        console.log("New Purchse!!");
+        console.log("\nNew Purchase!!");
 
         spendingThreshold = prompt("Enter your spending threshold:");
 
-        if(bankBalance < spendingThreshold){
+/*        if(bankBalance < spendingThreshold){
 
             alert("Spending Threshold can't be greater than Bank Balance!!!");
             continue;
 
         }
+*/
 
         priceOfPhone = prompt("What is the price of the phone you just purchased?");
 
@@ -40,20 +56,36 @@ if(bankBalance!==0){
         priceOfAccessory = spendingThreshold - priceOfPhone;
 
         if(priceOfAccessory == 0){
-            
-            alert("No accessories bought!");
+
+            console.log("No accessories sorted!");
+
+        }//else{
+
+           // console.log("The accesories costed ", priceOfAccessory);
+
+        //}
+
+        purchasePrice  += Number(spendingThreshold);
+        tax = taxAmount(purchasePrice);
+        purchaseReciept(Number(priceOfPhone), priceOfAccessory, purchasePrice, tax);
+
+        console.log("Can I afford it?");
+        if(bankBalance >= purchasePrice + tax){
+
+            console.log("Go ahead, you're rich!!!!");
+            bankBalance -= spendingThreshold;
+            totalPrice  += Number(priceOfPhone);
 
         }else{
             
-            console.log("The accesories costed ", priceOfAccessory);
+            console.log("Hah, in your dreams!");
 
         }
 
-        bankBalance -= spendingThreshold;
-        totalPrice  += Number(priceOfPhone);
     }
 
-    console.log("Total Price of all the phone purchses : ", totalPrice);     
+    console.log("\nTotal Price of all the phone purchses : ", totalPrice);   
+
 }else{
     alert("You're Broke!!!!");
 }
